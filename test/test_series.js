30ef5cf49cd7d73bceb7e555b583e5f06ffefd40
Lei Zongmin <leizongmin@gmail.com>
2012-12-28 16:54:31 +0800

    series及单元测试

---
+++ test/test_series.js
@@ -0,0 +1,75 @@
+/**
+ * 测试 if
+ */
+
+var assert = require('assert');
+var _ = require('../');
+
+describe('#if', function () {
+  
+  it('#series', function (done) {
+    var data = [];
+    _.series().do(function () {
+      data.push(1);
+      this.done();
+    }).do(function () {
+      data.push(2);
+      this.done();
+    }).do(function () {
+      data.push(3);
+      this.done();
+    }).end(function () {
+      assert.deepEqual(data, [1,2,3]);
+      done();
+    });
+  });
+  
+  it('#series - break', function (done) {
+    var data = [];
+    _.series().do(function () {
+      data.push(1);
+      this.done();
+    }).do(function () {
+      data.push(2);
+      this.break();
+    }).do(function () {
+      data.push(3);
+      this.done();
+    }).end(function () {
+      assert.deepEqual(data, [1,2]);
+      done();
+    });
+  });
+
+  it('#series - timeout - 1', function (done) {
+    var data = [];
+    _.series().do(function () {
+      data.push(1);
+      this.done();
+    }).do(function () {
+      data.push(2);
+      this.done();
+    }).do(function () {
+      data.push(3);
+    }).timeout(100).end(function () {
+      assert.deepEqual(data, [1,2,3]);
+      done();
+    });
+  });
+
+  it('#series - timeout - 2', function (done) {
+    var data = [];
+    _.series().do(function () {
+      data.push(1);
+      this.done();
+    }).do(function () {
+      data.push(2);
+    }).do(function () {
+      data.push(3);
+    }).timeout(100).end(function () {
+      assert.deepEqual(data, [1,2]);
+      done();
+    });
+  });
+
+})
