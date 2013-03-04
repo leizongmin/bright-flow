64ed05874918ed473352c1454beb513b2ef1193e
Lei Zongmin <leizongmin@gmail.com>
2013-03-04 17:20:49 +0800

    for支持timeout

--- test/test_for.js
+++ test/test_for.js
@@ -41,4 +41,36 @@ describe('#for', function () {
     });
   });
 
+  it('timeout - 1', function (done) {
+    var i = 0;
+    var data = [];
+    _.for(function () {
+      return i < 10;
+    }).do(function () {
+      data.push(i);
+      i++;
+      if (i < 5) this.done();
+    }).timeout(100).end(function (isTimeout) {
+      assert.equal(isTimeout, true);
+      assert.deepEqual(data, [0,1,2,3,4]);
+      done();
+    });
+  });
+
+  it('timeout - 2', function (done) {
+    var i = 0;
+    var data = [];
+    _.for(function () {
+      return i < 10;
+    }).do(function () {
+      data.push(i);
+      i++;
+      this.done();
+    }).timeout(100).end(function (isTimeout) {
+      assert.equal(isTimeout, false);
+      assert.deepEqual(data, [0,1,2,3,4,5,6,7,8,9]);
+      done();
+    });
+  });
+
 })
