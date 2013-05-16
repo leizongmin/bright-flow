26b81fda83a4d524eb19f49d500096d9226e6aea
Lei Zongmin <leizongmin@gmail.com>
2013-05-16 14:23:39 +0800

    文档

---
+++ doc/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Each",
+        "For",
+        "If",
+        "Parallel",
+        "Series"
+    ],
+    "modules": [
+        "bright-flow"
+    ],
+    "allModules": [
+        {
+            "displayName": "bright-flow",
+            "name": "bright-flow",
+            "description": "bright-flow"
+        }
+    ]
+} };
+});
