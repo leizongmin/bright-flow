95379593a1c3393d0f850700dd632d572c7902d2
Lei Zongmin <leizongmin@gmail.com>
2013-05-17 17:28:35 +0800

    改造 parallel

--- test/test_parallel.js
+++ test/test_parallel.js
@@ -21,8 +21,8 @@ describe('#parallel', function () {
     }).do(function () {
       sum += 8;
       this.done();
-    }).end(function (isTimeout) {
-      assert.notEqual(isTimeout, true);
+    }).end(function (err) {
+      assert.equal(err, null);
       assert.equal(sum, 15);
       done();
     });
@@ -43,15 +43,49 @@ describe('#parallel', function () {
     }).do(function () {
       sum += 8;
       setTimeout(this.done.bind(this), 100);
-    }).end(function (isTimeout) {
-      assert.notEqual(isTimeout, true);
+    }).end(function (err) {
+      assert.equal(err, null);
       assert.equal(sum, 15);
       assert.ok(Date.now() - timestamp < 150);
       done();
     });
   });
-
-  it('#parallel - break', function (done) {
+  
+  it('#parallel - break - 1', function (done) {
+    var sum = 0;
+    _.parallel().do(function () {
+      var me = this;
+      setTimeout(function () {
+        sum += 1;
+        me.done();
+      }, 10);
+    }).do(function () {
+      var me = this;
+      setTimeout(function () {
+        sum += 2;
+        me.done();
+      }, 10);
+    }).do(function () {
+      var me = this;
+      setTimeout(function () {
+        sum += 4;
+        me.break();
+      }, 10);
+    }).do(function () {
+      var me = this;
+      setTimeout(function () {
+        sum += 8;
+        me.done();
+      }, 10);
+    }).end(function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.code, _.BrightFlowError.BREAK);
+      assert.equal(sum, 7);
+      done();
+    });
+  });
+  
+  it('#parallel - break - 2', function (done) {
     var sum = 0;
     _.parallel().do(function () {
       sum += 1;
@@ -65,13 +99,14 @@ describe('#parallel', function () {
     }).do(function () {
       sum += 8;
       this.done();
-    }).end(function (isTimeout) {
-      assert.notEqual(isTimeout, true);
+    }).end(function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.code, _.BrightFlowError.BREAK);
       assert.equal(sum, 7);
       done();
     });
   });
-
+  
   it('#parallel - timeout - 1', function (done) {
     var sum = 0;
     _.parallel().do(function () {
@@ -85,8 +120,9 @@ describe('#parallel', function () {
       this.done();
     }).do(function () {
       sum += 8;
-    }).timeout(100).end(function (isTimeout) {
-      assert.equal(isTimeout, true);
+    }).timeout(100).end(function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.code, _.BrightFlowError.TIMEOUT);
       assert.equal(sum, 15);
       done();
     });
@@ -102,8 +138,9 @@ describe('#parallel', function () {
       sum += 4;
     }).do(function () {
       sum += 8;
-    }).timeout(100).end(function (isTimeout) {
-      assert.equal(isTimeout, true);
+    }).timeout(100).end(function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.code, _.BrightFlowError.TIMEOUT);
       assert.equal(sum, 15);
       done();
     });
